3a4b9da8d3a8972d2145deea6bfe990311452828
CodingZap <help@codingzap.com>
2025-09-16 15:42:13 +0530

    Create StockMarketApplication.java

---
+++ Real-Time Stock Market Monitoring/StockMarketApplication.java
@@ -0,0 +1,12 @@
+package com.example.stockmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class StockMarketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockMarketApplication.class, args);
+    }
+}
