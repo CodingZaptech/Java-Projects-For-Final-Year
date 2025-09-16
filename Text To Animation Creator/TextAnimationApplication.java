a91860378ef6aebd90670dbdfabb7f7f2db9ca29
CodingZap <help@codingzap.com>
2025-09-16 15:33:48 +0530

    Create TextAnimationApplication.java

---
+++ Text To Animation Creator/TextAnimationApplication.java
@@ -0,0 +1,12 @@
+package com.example.textanimation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class TextAnimationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TextAnimationApplication.class, args);
+    }
+}
