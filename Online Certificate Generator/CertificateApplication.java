9da0d2c48edd13a854a3c7ae749c50f9a8ebd4ae
CodingZap <help@codingzap.com>
2025-09-17 16:05:49 +0530

    Create CertificateApplication.java

---
+++ Online Certificate Generator/CertificateApplication.java
@@ -0,0 +1,12 @@
+package com.example.certificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class CertificateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CertificateApplication.class, args);
+    }
+}
