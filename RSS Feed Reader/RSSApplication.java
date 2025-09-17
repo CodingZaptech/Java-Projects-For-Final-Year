496f8c0255704a94aca59505bc974c537ccab5c7
CodingZap <help@codingzap.com>
2025-09-17 15:56:28 +0530

    Create RSSApplication.java

---
+++ RSS Feed Reader/RSSApplication.java
@@ -0,0 +1,12 @@
+package com.example.rss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class RSSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RSSApplication.class, args);
+    }
+}
