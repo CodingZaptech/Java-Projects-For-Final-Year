04f1dc6c79c1699ead6cac468a0e63030e214cc9
CodingZap <help@codingzap.com>
2025-09-17 15:19:00 +0530

    Create AirlineApplication.java

---
+++ Airline Reservation System/AirlineApplication.java
@@ -0,0 +1,12 @@
+package com.example.airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class AirlineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineApplication.class, args);
+    }
+}
