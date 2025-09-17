42df16fe2054a08e39565316abc997c21dbecbfa
CodingZap <help@codingzap.com>
2025-09-17 15:44:03 +0530

    Create BugTrackerApplication.java

---
+++ Error Or Bug Tracking System/BugTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.example.bugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class BugTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BugTrackerApplication.class, args);
+    }
+}
