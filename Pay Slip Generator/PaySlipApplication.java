4f96a746c106df0b0ababce7373cdb6f32286a23
CodingZap <help@codingzap.com>
2025-09-17 16:07:53 +0530

    Create PaySlipApplication.java

---
+++ Pay Slip Generator/PaySlipApplication.java
@@ -0,0 +1,12 @@
+package com.example.payslip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class PaySlipApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaySlipApplication.class, args);
+    }
+}
