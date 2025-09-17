7f79890fc9d9c14dde596cbbcf7dea8802c18736
CodingZap <help@codingzap.com>
2025-09-17 15:13:58 +0530

    Create ATMApplication.java

---
+++ ATM Monitoring And Management System/ATMApplication.java
@@ -0,0 +1,12 @@
+package com.example.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class ATMApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ATMApplication.class, args);
+    }
+}
