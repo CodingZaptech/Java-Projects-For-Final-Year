63b2690ec9b67602259cd6fcb253d2499c6db007
CodingZap <help@codingzap.com>
2025-09-17 14:58:59 +0530

    Create MiniGitHubApplication.java

---
+++ Mini Git Hub/MiniGitHubApplication.java
@@ -0,0 +1,12 @@
+package com.example.minigithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class MiniGitHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MiniGitHubApplication.class, args);
+    }
+}
