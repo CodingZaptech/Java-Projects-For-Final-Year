84880047c71861c4bf04af05cf68f5e8584e1a59
CodingZap <help@codingzap.com>
2025-09-17 16:03:27 +0530

    Create EmailSignatureApplication.java

---
+++ Email Signature Generator/EmailSignatureApplication.java
@@ -0,0 +1,12 @@
+package com.example.emailsignature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class EmailSignatureApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSignatureApplication.class, args);
+    }
+}
