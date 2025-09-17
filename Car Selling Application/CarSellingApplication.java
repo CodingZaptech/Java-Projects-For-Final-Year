066bd2c264e4fbedbe504b53dee8356ad627ffd6
CodingZap <help@codingzap.com>
2025-09-17 15:46:16 +0530

    Create CarSellingApplication.java

---
+++ Car Selling Application/CarSellingApplication.java
@@ -0,0 +1,12 @@
+package com.example.carselling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class CarSellingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarSellingApplication.class, args);
+    }
+}
