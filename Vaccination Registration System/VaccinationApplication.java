7f375034910be5f5678543dcf97dc8cd5dfa9eb3
CodingZap <help@codingzap.com>
2025-09-17 15:54:14 +0530

    Create VaccinationApplication.java

---
+++ Vaccination Registration System/VaccinationApplication.java
@@ -0,0 +1,12 @@
+package com.example.vaccination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class VaccinationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VaccinationApplication.class, args);
+    }
+}
