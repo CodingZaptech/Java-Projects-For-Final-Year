3c32bd0177ecdcf6dda91f7d35ccf64f8cb2451d
CodingZap <help@codingzap.com>
2025-09-17 15:11:41 +0530

    Create RestaurantBillingApplication.java

---
+++ Restaurant Billing Management System/RestaurantBillingApplication.java
@@ -0,0 +1,12 @@
+package com.example.restaurantbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main Spring Boot Application
+@SpringBootApplication
+public class RestaurantBillingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantBillingApplication.class, args);
+    }
+}
